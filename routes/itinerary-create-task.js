5594b2703e28df5c6d7a32bcf14138b6da159302
peiyu <seekn1234@gmail.com>
2023-07-17 15:52:59 +0800

    修改新增行程表單

--- routes/itinerary-create-task.js
+++ routes/itinerary-create-task.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 router.post("/", async (req, res) => {
-  //給予預設值
+  // 給予預設值
   let output = {
     redirect: "",
     totalRows: 0,
@@ -13,21 +13,19 @@ router.post("/", async (req, res) => {
     page: 1,
     rows: [],
   };
-  const perPage = 4 //每頁有４筆
+  const perPage = 4; // 每頁有４筆
 
   let page = req.query.page ? parseInt(req.query.page) : 1;
 
   let where = " WHERE 1 ";
- 
- 
 
-  const t_sql = `SELECT COUNT(1) totalRows FROM itinerary ${where}`//總筆數
+  const t_sql = `SELECT COUNT(1) totalRows FROM itinerary ${where}`; // 總筆數
   const [[{ totalRows }]] = await db.query(t_sql);
   let totalPages = 0;
   let rows = [];
   if (totalRows) {
     totalPages = Math.ceil(totalRows / perPage);
- 
+
     const sql = ` SELECT * FROM itinerary ${where} LIMIT ${
       perPage * (page - 1)
     }, ${perPage}`;
@@ -38,25 +36,29 @@ router.post("/", async (req, res) => {
 });
 
 // 新增資料的功能
-router.post('/',async(req,res)=>{
+router.post("/", async (req, res) => {
+  try {
+    // TODO: 要檢查的欄位
+    const sql =
+      "INSERT INTO `itinerary` " +
+      "(`img`, `name`, `date`, `description`, `public`, `ppl`, `note`, `create_at`) " +
+      "VALUES (?,?,?,?,?,?,?,?,NOW())";
 
-  //TODO:要檢查的欄位
-  const sql="INSERT INTO `itinerary`"+ "(`itin_id`, `img`, `date`, `name`, `description`, `public`, `ppl`, `member_id`, `note`, `create_at`)"+ "VALUES (?,?,?,?,?,?,?,?,?,NOW())"
+    const [result] = await db.query(sql, [
+      req.body.img,
+      req.body.date,
+      req.body.name,
+      req.body.description,
+    ]);
 
-
-  const[result]=await db.query(sql,[
-    req.body.img,
-    req.body.name,
-    req.body.date,
-    req.body.description,
-  ])
-
-  res.json({
-    result,
-    postData:req.body
-  })
-
-
-})
+    res.json({
+      result,
+      postData: req.body,
+    });
+  } catch (error) {
+    // 處理錯誤，回傳錯誤訊息給前端
+    res.status(500).json({ error: "無法新增資料到資料庫" });
+  }
+});
 
 module.exports = router;
