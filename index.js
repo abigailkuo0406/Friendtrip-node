a244ddb60ba53946577d995b2199de43247e58a6
HsiaoYun <asx13299@gmail.com>
2023-07-21 11:02:26 +0800

    productapi

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/products", require(__dirname + "/routes/example")); //主程式掛API示範
-
 //照片上傳（單張）
 app.post("/try-upload", upload.single("avatar"), (req, res) => {
   console.log(req.file);
@@ -133,8 +132,33 @@ app.post("/login", async (req, res) => {
   res.json(output);
 });
 
+// app.get("/product", async (req, res) => {
+//   let output = {};
+//   let rows = [];
+
+//   const sql = `SELECT * FROM products`;
+//   [rows] = await db.query(sql);
+
+//   output = {
+//     product_id: rows[0].product_id,
+//     product_name: rows[0].product_name,
+//     product_pirce: rows[0].product_pirce,
+//     product_brief: rows[0].product_brief,
+//     product_category: rows[0].product_category,
+//     product_launch: rows[0].product_launch,
+//     product_discon: rows[0].product_discon,
+//     product_main_img: rows[0].product_main_img,
+//     product_description: rows[0].product_description,
+//     product_post: rows[0].product_post,
+//     product_update: rows[0].product_update,
+//     product_upload: rows[0].product_upload,
+//   };
+//   return res.json(output);
+// });
+
 //設定靜態內容的資料夾(透過後端未經修改檔案都稱為靜態內容)
 app.get("*", express.static("public"));
+app.use("/product", require(__dirname + "/routes/product"));
 
 //自訂404頁面
 app.use((req, res) => {
