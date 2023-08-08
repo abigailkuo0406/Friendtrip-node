ea683d31373084a48e624a55c2f28ddd293a4ddc
abigailkuo <abigailkuo0406@gmail.com>
2023-08-09 05:05:25 +0800

    評論新增API

---
+++ routes/rest-comment.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const db = require(__dirname + "/../modules/mysql2")
+const router = express.Router()
+const upload = require(__dirname + "/../modules/img-upload")
+const multipartParser = upload.none()
+
+// 新增評論資料
+router.post("/addcomment", multipartParser, async (req, res) => {
+    const sql =
+        `INSERT INTO restcommment (
+            RestID, member_id, RestStarP, ComtText,created_time) VALUES ( ?, ?, ?, ?, NOW())`
+
+    const [result] = await db.query(sql, [
+        req.body.RestID,
+        req.body.member_id,
+        req.body.RestStarP,
+        req.body.ComtText,
+    ])
+
+    res.json({
+        result
+    })
+});
+module.exports = router;
