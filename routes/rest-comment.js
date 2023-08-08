7f53b7fc23236f9516d6340046a36dc5769cde9a
abigailkuo <abigailkuo0406@gmail.com>
2023-08-09 07:38:03 +0800

    評論讀取API

--- routes/rest-comment.js
+++ routes/rest-comment.js
@@ -4,11 +4,30 @@ const router = express.Router()
 const upload = require(__dirname + "/../modules/img-upload")
 const multipartParser = upload.none()
 
+// 讀取對應餐廳編號的資料
+router.post("/", multipartParser, async (req, res) => {
+    const sql = `SELECT 
+    ResComtID,
+    ComtRestId,
+    RestName,
+    comtMemberId,
+    member_name,
+    images,
+    rating, 
+    ComtText
+    FROM restcommment
+    JOIN member ON restcommment.comtMemberId = member.member_id
+    JOIN restaurant ON restcommment.ComtRestId = restaurant.RestID
+    WHERE ${req.body.restId}`;
+    const [rows] = await db.query(sql);
+    res.json(rows)
+})
+
 // 新增評論資料
 router.post("/addcomment", multipartParser, async (req, res) => {
     const sql =
         `INSERT INTO restcommment (
-            RestID, member_id, rating, ComtText,created_time) VALUES ( ?, ?, ?, ?, NOW())`
+        ComtRestId, comtMemberId, rating, ComtText, created_time) VALUES( ?, ?, ?, ?, NOW())`
 
     const [result] = await db.query(sql, [
         req.body.RestID,
