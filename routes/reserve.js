9cc6f73048e98b290d2dfea4caf2d8f883302256
abigailkuo <abigailkuo0406@gmail.com>
2023-08-10 02:55:16 +0800

    單筆訂位API欄位串接增加

--- routes/reserve.js
+++ routes/reserve.js
@@ -72,7 +72,7 @@ router.get("/:reserveRid", async (req, res) => {
     // 沒有 sid
     output.error = "沒有 rid !";
   } else {
-    const sql = `SELECT  reserve_member_id, reserveId,rest_id,RestName,RestPhone,RestAdress,RestImg,reserve_date,reserve_time,reserve_people
+    const sql = `SELECT  reserve_member_id, reserveId,rest_id,RestName,RestPhone,RestAdress,RestImg,reserve_date,reserve_time,reserve_people,state
     FROM reserve
     JOIN restaurant ON reserve.rest_id = restaurant.RestID
     WHERE reserveId=${reserveRid}`;
