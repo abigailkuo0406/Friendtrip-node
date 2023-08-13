e1239826fff36ab31d0de4654f4d3d80b22d4795
abigailkuo <abigailkuo0406@gmail.com>
2023-08-14 03:59:35 +0800

    交友邀請資料讀取API

--- routes/friends.js
+++ routes/friends.js
@@ -5,15 +5,32 @@ const upload = require(__dirname + "/../modules/img-upload");
 const multipartParser = upload.none();
 
 router.post("/", async (req, res) => {
-  const sql = `SELECT 
-  memberId,
-  FriendId,
-  member_name,images
-  FROM friends
-  INNER JOIN member ON friends.FriendId = member.member_id
-  WHERE memberId = ${req.body.memberID} && acceptState=0`;
-  const [rows] = await db.query(sql);
-  res.json({ all: rows });
+  let output = {
+    totalUnaccepts: 0,
+  };
+  const t_sql = `SELECT COUNT(1) totalUnaccepts FROM friends WHERE FriendId = ${req.body.memberID} && acceptState=${req.body.acceptState}`; // 查詢好友總數
+  const [[{ totalUnaccepts }]] = await db.query(t_sql); //總筆數
+
+  let rows1 = []; //資料陣列
+  let rows2 = []; //資料陣列
+
+
+  const sql1 = `SELECT memberId, FriendId, member_name,images FROM friends INNER JOIN member ON friends.FriendId = member.member_id WHERE memberId = ${req.body.memberID} && acceptState=${req.body.acceptState}`;
+  [rows1] = await db.query(sql1);
+
+  const sql2 = `SELECT memberId, FriendId, member_name,images FROM friends INNER JOIN member ON friends.memberId = member.member_id WHERE FriendId = ${req.body.memberID} && acceptState=${req.body.acceptState}`;
+  [rows2] = await db.query(sql2);
+
+  const rows = [...rows1, ...rows2]
+
+  output = {
+    ...output,
+    totalUnaccepts,
+    rows,
+    rows2
+  };
+  return res.json(output);
+
 });
 
 // 修改好友狀態(接受邀請)
@@ -22,7 +39,7 @@ router.put("/edit", multipartParser, async (req, res) => {
   const FriendId = req.body.FriendId;
 
   // 修改指定好友關係
-  const sql = `UPDATE friends SET acceptState=1 WHERE memberId=${memberId} && FriendId=${FriendId}`;
+  const sql = `UPDATE friends SET acceptState=1 WHERE memberId=${FriendId} && FriendId=${memberId}`;
   const [result1] = await db.query(sql);
 
 
@@ -36,7 +53,7 @@ router.put("/edit", multipartParser, async (req, res) => {
 router.delete("/delete", multipartParser, async (req, res) => {
   const memberId = req.body.memberID;
   const FriendId = req.body.FriendId;
-  const sql = `DELETE FROM friends WHERE friends.memberId =${memberId} AND friends.FriendId = ${FriendId}`
+  const sql = `DELETE FROM friends WHERE friends.memberId =${FriendId} AND friends.FriendId = ${memberId}`
   const [result] = await db.query(sql)
   res.json({
     result: result,
